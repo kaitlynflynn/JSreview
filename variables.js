85f5dbbc1052436d736e997b9da71a197cdf9c7c
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-16 13:48:07 -0800

    Created new file, variables.js

---
+++ variables.js
@@ -0,0 +1,5 @@
+// Created variables = to value of 7 (x) and 3 (y) 
+let x = 7;
+let y = 3;
+let z = x + y; // + sign = addition operator in this line 
+console.log('Answer: ' + z); // + sign here strings two values together/concatenate the values of x + y
