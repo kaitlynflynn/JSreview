a99ead2653b6d887ed1dbc8fd54b3da88e9d5045
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-16 19:52:15 -0800

    Fixed typo

--- coercion.js
+++ coercion.js
@@ -1,7 +1,7 @@
 let a = 7;
 let b = '6'; 
 b = parseInt(b, 10); // parseInt is a built in function to JS which forces the string to become a number so we can add them together to print 13. 
-let c = a + b; // + operator is concatinating a & b together to print out 76 (versus adding them together to be 13). 
+let c = a + b; // + operator is concatenating a & b together to print out 76 (versus adding them together to be 13). 
 console.log('Answer: ' + c);
 
 let d = parseInt('b', 10);
