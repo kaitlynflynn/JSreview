91a7c73eb9bc9ed9b2ef25d0ad3b87a5bf948b12
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-16 16:12:18 -0800

    Added some detailed notes to variables.js

--- variables.js
+++ variables.js
@@ -1,5 +1,21 @@
+// What is a variable? 
+// Variable is an area in the computers memory to declare a value or put info and retrieve info.
+
+// What is a keyword? 
+// 'let' is a keyword and so is 'var'. 'let' is a newer keyword. 
+ 
+// What is 'const'? 
+// 'const' means the value declared will remain constant throughout the code, meaning it never changes.
+
+// What does the equal sign mean in the below? 
+// This is called an assignment operator and how we assign a value into a variable. 
+
 // Created variables = to value of 7 (x) and 3 (y) 
 let x = 7;
+x = 6; 
+x = 5;
+x = 4; // when assigning new values to 'x', this problem will then print '7' to the console since 4 + 3 = 7, and 4 was the last value assigned to 'x'. 
+// You can only declare a variable once, but you can assign a new value as many times as you want after that.  
 let y = 3;
 let z = x + y; // + sign = addition operator in this line 
 console.log('Answer: ' + z); // + sign here strings two values together/concatenate the values of x + y
