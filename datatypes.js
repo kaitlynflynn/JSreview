52cd3ee10ba66b64b7ebd8ee387aed9cc31a9fe8
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-16 16:33:52 -0800

    Added sample problems with notes for 4 different datatypes

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+let x = 7;
+console.log(typeof x); // this prints to console the word 'number'. 
+
+let y = true; // true or false
+console.log(typeof y); // this prints to console the word 'boolean'.
+
+let z = 'hello world'; // single quotes
+console.log(typeof z); // this prints to console the word 'string'.
+
+let a; 
+console.log(a); // prints to console the word 'undefined'.
+console.log(typeof a); // prints to console the word 'undefined'.
+
+// Different types of datatypes: 
+// 1. Number - If you want to perform any type of math.
+// 2. Boolean - If you want to do yes/no or true/false evaluation.
+// 3. String - If you want to display something on screen (string is shorthand for 'string of characters')
+// 4. undefined 
