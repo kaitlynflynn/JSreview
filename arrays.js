a1f4566c016975f8742edd1ad181faed31bb61c9
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-17 14:21:29 -0800

    Added new file arrays.js along with a sample problem/notes

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// An Array is basically a variable that can hold many different values
+
+let a = [4, 8, 15, 16, 23, 42]; // an array of numeric values 
+let b = [ 'david', 'eddie', 'alex', 'michael' ]; // an array of strings
+
+// NOTE: Index starts at 0, so in the above array, 4 is at index 0, 8 is at index 1 and so on
+console.log(a[0]); // prints value of the array located at index 0, printing 4
+console.log(a[1]); // prints value of the array located at index 2, printing 8
+console.log(a); // prints the entire contents of the array, printing [ 4, 8, 15, 16, 23, 42 ]
