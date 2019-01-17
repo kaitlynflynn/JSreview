b88e0fa910a442eb2cdf7ec9ebfe7c223ae997ce
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-17 13:45:39 -0800

    Added some examples and notes to operators.js

--- operators.js
+++ operators.js
@@ -1,2 +1,34 @@
 // Categories of Operators 
 
+// Assignment = 
+// Arithmetic + - * / % (addition, subtraction, multiplication, division, modulus)
+var m = 10 % 3;
+console.log(m); // Console prints the remainder of 1; 10 divided by 3 = 3 with 1 left over
+
+// Increment / Decrement ++ -- 
+var a = 1;
+a++; // this increments the value of a
+console.log(a++); // console prints 2
+console.log(++a); // ++ before the a means it first evaluates the increment of a and then prints to console
+console.log(a); // console prints 3
+
+// String '' +
+
+// Precedence
+var b = 1 + 2 * 3; // multiplication takes place first in order of operations so 2 * 3 + 1 will print 7 to console. 
+console.log(b);
+
+var b = (1 + 2) * 3; // in this example the parenthesis tell us to add 1 + 2 together first, then * 3 printing 9 to console.
+console.log(b);
+
+// Function invocation operators are the parenthesis ()
+console.log('')
+
+// Logical and: && or: ||
+
+// Member accessor operator is the period after console
+console.log()
+
+// Code block operators { }
+
+// Array element access operators []
