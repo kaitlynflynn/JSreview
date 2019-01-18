b2020a8e84212581ea61216ef9aed1d618aa76b2
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-18 15:53:52 -0800

    Added function expression sample problem

---
+++ function-expressions.js
@@ -0,0 +1,5 @@
+// Created a function expression and at end of declaration, give it an argument, in this case, 2000
+// NOTE: functions can take functions as input parameters 
+setTimeout(function () { 
+     console.log('I waited 2 seconds'); // this successfully prints to terminal after waiting 2 seconds
+    }, 2000); // 2000 = number of milliseconds 
