a1e996c00e5460e3b8ac08c90addafbd3f854358
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-18 18:15:55 -0800

    Added sample problems to demo for and while loops

--- iterations.js
+++ iterations.js
@@ -1,1 +1,23 @@
 // iteration statements - allows us to loop through a body/block of code a number of times until a certain condition is met
+for (let i = 0; i < 10; i++) { // 3 parts here, declared variable i & initializing value to 0; as long as i is less than 10, continue with the loop; then increment value of i by 1 
+    console.log(i); // prints 0 through 9
+// }
+
+// // example 2 with for loop
+let a = [4, 8, 15, 16, 23, 42]; // declaring variable with an array of numeric values
+for (i = 0; i < a.length; i++) { // for loop, i starts with value of 0, then checks if i is less than the length of the array (6) & iterates through
+    console.log(a[i]); // prints our list
+// }
+
+// snippet structure within JS - helpful for letting you rename counters, arrays & element
+let a = [4, 8, 15, 16, 23, 42];
+for (let b = 0; b < a.length; b++) {
+    const c = a[b];
+    console.log(c);
+}
+
+// while loop
+let x = 1;
+while (x < 10) {
+    console.log(x++); // prints out the value of x and increments by 1 until it gets to 10; prints 1-9
+}
