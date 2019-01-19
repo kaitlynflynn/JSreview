627bca615b4c8d8844bce6cff2961a8df4c3c1a2
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-19 14:59:02 -0800

    Added 3 sample problems and notes to demo destructuring

---
+++ destructuring.js
@@ -0,0 +1,39 @@
+// declare some variables
+let a, b, c, d, e; 
+
+// array of names
+let names = [ 'comrade', 'clementine', 'kevin', 'chairman', 'kaitlyn' ];
+
+// destructure names array into a set of variables:
+[a, b, c, e, d] = names;
+
+console.log(a); // prints comrade
+console.log(b); // prints clementine
+console.log(c); // prints kevin
+console.log(d); // prints kaitlyn
+console.log(e); // prints chairman
+
+//-------------------------------------------
+// another example: 
+let others;
+
+[a, b, ...others] = names; // we can take elements one by one or contain other elements together by utilizing the ...others
+
+console.log(a); // prints comrade
+console.log(b); // prints clementine
+console.log(others); // prints [ 'kevin', 'chairman', 'kailtyn ]
+
+
+//-------------------------------------------
+// another example: 
+// this is a compact syntax that helps to clean up code whenever you're trying to map from one data structure into another or another set of variables
+let year, model;
+({year, model} = {
+    make: 'bmw',
+    model: '335is',
+    year: 2011,
+    value: 5000
+});
+
+console.log(year); // prints 2011
+console.log(model); // prints 335is
