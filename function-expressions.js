fa6af388a4b6bc240971f66d8f9600de19238748
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-18 16:04:31 -0800

    Added an example for an IIFE problem

--- function-expressions.js
+++ function-expressions.js
@@ -1,8 +1,8 @@
 // Created a function expression and at end of declaration, give it an argument, in this case, 2000
 // NOTE: functions can take functions as input parameters 
-// setTimeout(function () { 
-//      console.log('I waited 2 seconds'); // this successfully prints to terminal after waiting 2 seconds
-//     }, 2000); // 2000 = number of milliseconds 
+setTimeout(function () { 
+     console.log('I waited 2 seconds'); // this successfully prints to terminal after waiting 2 seconds
+    }, 2000); // 2000 = number of milliseconds 
 
 
 
@@ -16,3 +16,7 @@ function timeout() {
 
 timeout(); // calling the function declaration once
 // ctrl + c to stop the execution on terminal - will keep going every 2 seconds until you do
+
+(function () {
+    console.log('Immediately Invoked Function Expression (IIFE');
+})();
